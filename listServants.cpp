1a5cbfdb5ae8086faa1d95d5ac8b9005e4f24f8b
orkfpot <ignizz0@gmail.com>
2024-02-26 13:44:27 +0000

    added broken code and data for servants

---
+++ listServants.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::ifstream fp;
+    fp.open("servants.json");
+	std::string line;
+	if(!fp.is_open())
+	{
+		std::cout << "failed to open file" << std::endl;
+		return -1;
+	}
+	int x=0;
+
+	while(getline(fp,line))
+	{  
+
+
+		if(line.find("battleName") != std::string::npos && line.find("        ") == std::string::npos)
+        {
+            x++;
+            line.erase (0, 19);
+	    line.erase (line.end()-2, 2);
+            std::cout << line << std::endl;
+        }
+    }
+}
