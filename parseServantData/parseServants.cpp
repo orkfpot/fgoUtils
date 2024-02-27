5393e91f6304990ca8bd25b26c78d43fec35177a
orkfpot <ignizz0@gmail.com>
2024-02-27 16:31:05 +0000

    start to parsing the json file on a character by character basis

---
+++ parseServantData/parseServants.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main()
+{
+	std::ifstream fp("servants.json");
+	std::string searchSubject;
+	char b;
+	if(!fp.is_open())
+	{
+		std::cout << "failed to open file" << std::endl;
+		return -1;
+	}
+	int x=0, y=0;
+
+
+
+	if(!fp.get(b))
+		std::cout << "NULL file pointer\n";
+	
+mainLoop:
+	if(b=='{')
+	{
+		y++;
+		x=y-1;
+		goto indent;
+	}
+	if(b=='}')
+	{
+		y--;
+		x=y;
+		goto indent;
+	}
+	std::cout << b;
+	goto skipIndent;
+indent:
+
+	std::cout << std::endl;
+	for(int i = 0; i < x; i++)
+		std::cout << "    ";
+	std::cout << b << std::endl;
+	for(int i = 0; i < y; i++)
+		std::cout << "    ";
+skipIndent:
+	if(fp.get(b))
+		goto mainLoop;
+
+/* irrelevant code as I am changing it to a character by character reading of file
+	while(getline(fp,line))
+	{
+
+
+		if(line.find(searchSubject) != std::string::npos)
+			goto saveInfo;
+		if(line.find("{") != std::string::npos)
+			x++;
+		if(line.find("}") != std::string::npos)
+		{
+			x--;
+		}
+	}
+	std::cout << "servant not found" << std::endl;
+	return -1;
+saveInfo:
+	this->name = searchSubject;
+	while(getline(fp,line))
+	{
+		std::cout << line << std::endl;
+
+
+		if(line.find("{") != std::string::npos)
+			x++;
+		if(line.find("}") != std::string::npos)
+		{
+			x--;
+			if(x==0)
+				break;
+		}
+	}
+	std::cout << name << std::endl;
+*/
+
+	return 0;
+}
