f0d0dc904cda2e2e5975a5cf3ac4b2582b9e6469
orkfpot <ignizz0@gmail.com>
2024-03-08 15:30:19 +0000

    writes out some data formatted somewhat reasonably but it isn't the data that I want

--- parseServantData/parseServants.cpp
+++ parseServantData/parseServants.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
 
 #define WAIT_FOR_VAL_NAME 0
 #define READ_VAR_NAME 1
@@ -8,191 +9,83 @@
 #define READ_VAR_VAL 3
 
 // god bless tellg(), seekg() and read()
-int parseServants(std::ifstream &fp, int x = 0, int y = 0)
+int parseServants(std::ifstream &fp, int x = 0)
 {
 	char b;
-	int charState;
 	unsigned long int pos, pos2;
+	int y = x;
+	int charState;
 	int posDiff;
-	while(fp.get(b))
+
+skipWrite:
+while(fp.get(b))
 	{
-		
-		switch(b)
+	
+	switch(b)
+	{
+	case '{':
+		y++;
+		x=y-1;
+		break;
+	case '}':
+		y--;
+		x=y;
+		break;
+	case '"':
+		if(charState==READ_VAR_NAME)
 		{
-			case '{':
-				y++;
-				x=y-1;
-				break;
-			case '}':
-				y--;
-				x=y;
-				break;
-			case '"':
-				if(charState==READ_VAR_NAME)
-				{
-					char string[posDiff];
-					fp.seekg(pos);
-					fp.read(string, posDiff);
-					fp.seekg(pos2);
-					pos=fp.tellg();
-					charState=WAIT_FOR_VAR_VAL;
-				}
-				else
-				{
-					pos2 = fp.tellg();
-					posDiff=(pos2-pos);
-					char string[posDiff];
-					fp.seekg(pos);
-					fp.read(string, posDiff);
-					fp.seekg(pos2);
-					charState=READ_VAR_NAME;
-				}
-				break;
-			case ':':
-				charState=READ_VAR_VAL;
-				break;
-			case ',':
-				charState=WAIT_FOR_VAL_NAME;
-				std::cout << '\n';
-				break;
+			charState=WAIT_FOR_VAR_VAL;
+			goto write;
+		}
+		else
+		{
+			charState=READ_VAR_NAME;
+			pos = fp.tellg();
+		}
+		break;
+	case ':':
+		charState=READ_VAR_VAL;
+		pos = fp.tellg();
+		break;
+	case ',':
+		charState=WAIT_FOR_VAL_NAME;
+		goto write;
+		break;
 
 
-			default:
-				std::cout << b;
-				break;
-		}
-		
-		/*
-		switch(charState)
-		{
-			case READ_VAR_NAME:
-				fp.seekg(pos);
-				fp.read(string, posDiff);
-				fp.seekg(pos2);
-				break;
-			case READ_VAR_VAL:
-				fp.seekg(pos);
-				fp.read(string, posDiff);
-				fp.seekg(pos);
-				break;
-			default:
-				break;
-		}
-		*/
+	default:
+//		std::cout << b;
+		break;
+	}
+
+	goto skipWrite;
+
+write:
+	pos2 = fp.tellg();
+	posDiff=(pos2-pos);
+	char string[posDiff]; // I do need to free this don't I? after all I create a new one every iteration of the loop and this loop. or does it go out of scope every loop
+	fp.seekg(pos);
+	fp.read(string, posDiff);
+	fp.seekg(pos2);
+	std::cout << string << '\n';
+
 	}
 	return 0;
 }
 
-/*
-std::string parseServants(&fp)
-{
-
-// this is gonna have to become a loop and a recursive function
-
-/*
- * read file as done here until reach :
- * when encountered : call self?
- *
- * is that really all?
- *
-
-// gonna rewrite this a second time
-mainLoop:
-	switch(b)
-	{
-		case '{':
-			y++;
-			x=y-1;
-			
-			break;
-		case '}':
-			y--;
-			x=y;
-			break;
-		case '"':
-			if(charState==READ_VAR_NAME)
-				charState=WAIT_FOR_VAR_VAL;
-			else
-				charState=READ_VAR_NAME;
-			break;
-		case ':':
-			charState=READ_VAR_VAL;
-			break;
-		case ',':
-			charState=WAIT_FOR_VAL_NAME;
-			std::cout << '\n';
-			break;
-
-
-		default:
-			std::cout << b;
-			goto skipWrite;
-	}
-
-write:
-	switch(charState)
-	{
-		case READ_VAR_NAME:
-			std::cout << b;
-			break;
-		case READ_VAR_VAL:
-			std::cout << b;
-		default:
-			break;
-	}
-	
-skipWrite:
-	if(fp.get(b))
-		goto mainLoop;
-}
-*/
 int main()
 {
 	std::ifstream fp("servants.json");
 	std::string searchSubject;
 	char b;
+
 	if(!fp.is_open())
 	{
 		std::cout << "failed to open file" << '\n';
 		return -1;
 	}
+
 	parseServants(fp);
-
-
-/* irrelevant code as I am changing it to a character by character reading of file
-	while(getline(fp,line))
-	{
-
-
-		if(line.find(searchSubject) != std::string::npos)
-			goto saveInfo;
-		if(line.find("{") != std::string::npos)
-			x++;
-		if(line.find("}") != std::string::npos)
-		{
-			x--;
-		}
-	}
-	std::cout << "servant not found" << '\n';
-	return -1;
-saveInfo:
-	this->name = searchSubject;
-	while(getline(fp,line))
-	{
-		std::cout << line << '\n';
-
-
-		if(line.find("{") != std::string::npos)
-			x++;
-		if(line.find("}") != std::string::npos)
-		{
-			x--;
-			if(x==0)
-				break;
-		}
-	}
-	std::cout << name << '\n';
-*/
 
 	return 0;
 }
